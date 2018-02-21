caff91d63ae17a1c3049cb3a62e4c87a3b4bd3c2
Germán Küber <german.kuber@outlook.com>
2018-02-21 10:36:03 -0300

    Refactor in one static extension method

---
+++ RefactoringExercisePatternsDesign/RefactoringExercisePatternsDesign/IntExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace RefactoringExercisePatternsDesign
+{
+    public static class IntExtension
+    {
+        public static IEnumerable<int> DigitsFromLowest(this long number)
+        {
+            do
+            {
+                yield return (int) number % 10;
+                number /= 10;
+
+            } while (number >0);
+        }
+    }
+}
--- RefactoringExercisePatternsDesign/RefactoringExercisePatternsDesign/Program.cs
+++ RefactoringExercisePatternsDesign/RefactoringExercisePatternsDesign/Program.cs
@@ -13,21 +13,12 @@ namespace RefactoringExercisePatternsDesign
         }
 
         static int GetControlDigit(long number) =>
-            GetDigitsOf(number)
+            number
+                    .DigitsFromLowest()
                         .Zip(MultiplyingFactors, (a, b) => a * b)
                         .Sum() % 7;
 
-        private static IList<int> GetDigitsOf(long number, IEnumerator<int> factor)
-        {
-            IList<int> pondereDigits = new List<int>();
-            foreach (var digit in GetDigitsOf(number))
-            {
-                factor.MoveNext();
-                pondereDigits.Add(digit * factor.Current);
-            }
 
-            return pondereDigits;
-        }
 
         public static IEnumerable<int> MultiplyingFactors
         {
@@ -43,17 +34,6 @@ namespace RefactoringExercisePatternsDesign
             }
         }
 
-        private static IEnumerable<int> GetDigitsOf(long number)
-        {
 
-            IList<int> digits = new List<int>();
-            while (number > 0)
-            {
-                digits.Add((int)(number % 10));
-                number /= 10;
-            }
-
-            return digits;
-        }
     }
 }
