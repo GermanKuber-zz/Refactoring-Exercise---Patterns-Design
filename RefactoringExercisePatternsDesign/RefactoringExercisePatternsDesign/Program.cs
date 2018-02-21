41ceda234bafda8b3241d6033a787b0edac77dbf
Germán Küber <german.kuber@outlook.com>
2018-02-21 01:01:00 -0300

    First commit

---
+++ RefactoringExercisePatternsDesign/RefactoringExercisePatternsDesign/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RefactoringExercisePatternsDesign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetControlDigit(989));
+        }
+
+        static int GetControlDigit(long number)
+        {
+            int sum = 0;
+            bool isOddPos = true;
+
+            while (number >0)
+            {
+                int digit = (int)(number % 10);
+                if (isOddPos)
+                    sum += 3 * digit;
+                else
+                    sum += digit;
+                number /= 10;
+                isOddPos = !isOddPos;
+            }
+
+            int modulo = sum % 7;
+            return modulo;
+
+        }
+    }
+}
